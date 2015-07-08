94052e19d7a21d496f469693c223a0d0f3cac833
Mathis Klooß <mathis@mage-profis.de>
2015-07-08 09:55:58 +0200

    initial release

---
+++ src/app/code/community/MageProfis/EmailCheck/Model/Observer.php
@@ -0,0 +1,52 @@
+<?php
+
+class MageProfis_EmailCheck_Model_Observer
+{
+    
+    protected $_orderStates = array('pending', 'processing', 'new');
+    
+    public function sendOrderEmail()
+    {
+        $dateFrom = date('Y-m-d H:i:s', strtotime('-3 days'));
+        $dateTo = date('Y-m-d H:i:s', strtotime('-15 min'));
+        $collection = Mage::getModel('sales/order')->getCollection()
+                ->addFieldToSelect(array('entity_id'))
+                ->addFieldToFilter('email_sent', 0)
+                ->addFieldToFilter('state', array('in' => $this->_orderStates))
+                ->addFieldToFilter('created_at', array(
+                    'from' => $dateFrom,
+                    'to' => $dateTo,
+                ))
+        ;
+        foreach($collection->getAllIds() as $_id)
+        {
+            $order = Mage::getModel('sales/order')->load($_id);
+            /** @var Mage_Sales_Model_Order $order */
+            
+            // get next item, if email_sent is set or state has been changed!
+            if( (int) $order->getEmailSent() == 1 && !in_array($order->getSate(), $this->_orderStates))
+            {
+                continue;
+            }
+
+            $payment = $order->getPayment();
+            /** @var Mage_Sales_Model_Order_Payment $payment */
+            if($payment && $payment->getId())
+            {
+                $instance = $payment->getMethodInstance();
+                if($instance && in_array($instance->getCode(), array('paypal_standard', 'paypal_express', 'hpcc', 'hpsu')) && $order->getState() != 'processing')
+                {
+                    continue;
+                }
+            }
+
+            try {
+                $order->sendNewOrderEmail();
+                Mage::log('Order E-Mail has been send: '. $order->getIncrementId(), null, 'orderemail.log', true);
+            } catch(Exception $e)
+            {
+                Mage::log('Error While Sending E-Mail: '. $order->getIncrementId(), null, 'orderemail.log', true);
+            }
+        }
+    }
+}
